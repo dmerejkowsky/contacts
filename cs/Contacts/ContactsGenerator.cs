32c327d3a3c0bfb3fd090b7456fb502b6a033241
Dimitri Merejkowsky <dimitri@dmerej.info>
2025-01-21 09:33:09 +0100

    cs: better values in GenerateContacts method

--- cs/Contacts/ContactsGenerator.cs
+++ cs/Contacts/ContactsGenerator.cs
@@ -10,8 +10,9 @@ public class ContactsGenerator
   {
     // TODO: generate `count` contacts without
     // filling up memory - maybe using IEnemerable ?
-    Contact c1 = new("Alex", "contact-1@domain.tld");
-    Contact c2 = new("Bev", "contact-2@domain.tld");
+    Contact c1 = new("name-1", "email-1@domain.tld");
+    Contact c2 = new("name-2", "email-2@domain.tld");
+    Contact c3 = new("name-3", "email-3@domain.tld");
     return new List<Contact> { c1, c2 };
   }
 }
